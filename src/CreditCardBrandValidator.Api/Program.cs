a554125ca201b59a4cf89602881b38274defc881
alexsandev <alex.rsjr10@gmail.com>
2025-12-28 11:50:13 -0300

    Configurando serviços e criando endpoint 'ValidateCard' na camada de API.

--- src/CreditCardBrandValidator.Api/Program.cs
+++ src/CreditCardBrandValidator.Api/Program.cs
@@ -1,8 +1,18 @@
+using CreditCardBrandValidator.Application.DTOs;
+using CreditCardBrandValidator.Application.Interfaces;
+using CreditCardBrandValidator.Application.Services;
+using CreditCardBrandValidator.Domain.Interfaces;
+using CreditCardBrandValidator.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<ICardAppService, CardAppService>();
+builder.Services.AddScoped<ICardDomainService, CardDomainService>();
 
 var app = builder.Build();
 
@@ -14,28 +24,17 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/ValidateCard", ([FromQuery]string cardNumber, [FromServices]ICardAppService cardAppService) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    if (string.IsNullOrEmpty(cardNumber))
+    {
+        return Results.BadRequest("Card number must be provided.");
+    }
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var response = cardAppService.ExecuteCardValidation(new CardRequestDTO(cardNumber));
+    
+    return Results.Ok(response);
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
