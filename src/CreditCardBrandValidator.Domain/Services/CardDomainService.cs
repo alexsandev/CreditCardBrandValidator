71532c266f4480ca1d26ddb6781cd5709bd46d5c
alexsandev <alex.rsjr10@gmail.com>
2025-12-28 20:27:01 -0300

    Otimizando a classe 'CardDomainService'.

--- src/CreditCardBrandValidator.Domain/Services/CardDomainService.cs
+++ src/CreditCardBrandValidator.Domain/Services/CardDomainService.cs
@@ -5,39 +5,41 @@ namespace CreditCardBrandValidator.Domain.Services;
 
 public class CardDomainService : ICardDomainService
 {
-    private static readonly Dictionary<string, string> CardPatterns = new()
+    private static readonly List<(string pattern, string brand)> CardPatterns = new()
     {
-        { @"^4[0-9]{12}(?:[0-9]{3})?$", "Visa" },
-        { @"^5[1-5][0-9]{14}$|^2(?:22[1-9]|2[3-9][0-9]|[3-6][0-9]{2}|7[01][0-9]|720)[0-9]{12}$", "Mastercard" },
-        { @"^((457631|457632|438935|636368|636297|504175|5067|5090|627780|636297)[0-9]*)", "Elo" },
-        { @"^3[47][0-9]{13}$", "American Express" },
-        { @"^606282[0-9]{10}$|^3841[0-9]{15}$", "Hipercard" }
+        (@"^((457631|457632|438935|636368|636297|504175|5067|5090|627780|636297)[0-9]*)", "Elo"),
+        (@"^4[0-9]{12}(?:[0-9]{3})?$", "Visa"),
+        (@"^5[1-5][0-9]{14}$|^2(?:22[1-9]|2[3-9][0-9]|[3-6][0-9]{2}|7[01][0-9]|720)[0-9]{12}$", "Mastercard"),
+        (@"^3[47][0-9]{13}$", "American Express"),
+        (@"^606282[0-9]{10}$|^3841[0-9]{15}$", "Hipercard")
     };
 
-    public (bool isValid, string brand) ValidateCard(string cardNumber)
+    public (bool isValid, string brand) ValidateCard(string? cardNumber)
     {
-        if (string.IsNullOrWhiteSpace(cardNumber) || cardNumber.Length > 30)
+        if (string.IsNullOrWhiteSpace(cardNumber) || cardNumber.Length < 13 || cardNumber.Length > 30)
             return (false, "Unknown");
 
         var cleanNumber = Regex.Replace(cardNumber, @"[^\d]", "");
 
-        if (cleanNumber.Length < 12 || cleanNumber.Length > 19)
+        if (cleanNumber.Length < 13 || cleanNumber.Length > 50)
             return (false, "Unknown");
 
         var brand = GetCardBrand(cleanNumber);
+        var isValidLuhn = ValidateLuhn(cleanNumber);
 
-        var isValid = ValidateLuhn(cleanNumber) && brand != "Unknown";
+        if (!isValidLuhn || brand == "Unknown")
+            return (false, "Unknown");
 
-        return (isValid, brand);
+        return (true, brand);
     }
 
     private string GetCardBrand(string cardNumber)
     {
-        foreach (var pattern in CardPatterns)
+        foreach (var (pattern, brand) in CardPatterns)
         {
-            if (Regex.IsMatch(cardNumber, pattern.Key, RegexOptions.None, TimeSpan.FromMilliseconds(100)))
+            if (Regex.IsMatch(cardNumber, pattern, RegexOptions.None, TimeSpan.FromMilliseconds(100)))
             {
-                return pattern.Value;
+                return brand;
             }
         }
 
